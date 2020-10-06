38f76bb58603bd053ad500459385512f2bd65a49
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-05 21:04:10 -0500

    added users.json

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import userData from './farmerData';
+import userData from './userData';
 import auth from '../../components/auth/auth';
 import myNavbar from '../../components/myNavbar/myNavbar';
 import viewHelper from '../viewHelpers';
 
 const checkLoginStatus = () => {
-  firebase.auth().onAuthStateChanged((farmer) => {
-    if (farmer) {
-      const currentFarmer = userData.setCurrentFarmer(farmer);
-      myNavbar.myNavbar(currentFarmer);
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      const currentUser = userData.setCurrentUser(user);
+      myNavbar.myNavbar(currentUser);
       viewHelper.viewListener('cows-link');
     } else {
       auth.loginButton();
