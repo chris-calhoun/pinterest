81819de4a8d749d0d010ca73616b6733909eec76
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-08 23:54:25 -0500

    connecting pins to appropriate boards,

--- src/javascripts/components/cards/boardCard.js
+++ src/javascripts/components/cards/boardCard.js
@@ -1,14 +1,13 @@
-import pinsView from '../views/pinsView';
-
+// import pinsView from '../views/pinsView';
 const boardMaker = (boardObject) => {
-  const domString = `<div class="card m-2" style="width: 18rem;" id="${boardObject.firebaseKey}">
+  const domString = `<div class="card m-2" style="width: 18rem;" id="${boardObject.Board_Id}">
     <div class="card-body">
     <img class="card-img-top" src="${boardObject.Image_URL}" alt="Card image cap">
       <h5 class="card-title">${boardObject.Name}</h5>
       </div>
   </div>`;
 
-  pinsView.showBoardPins();
+  // pinsView.showBoardPins(boardObject.Board_Id);
   return domString;
 };
 
--- src/javascripts/components/views/pinsView.js
+++ src/javascripts/components/views/pinsView.js
@@ -13,11 +13,24 @@ const showAllPins = (uid) => {
   });
 };
 
-const showBoardPins = () => {
+const showBoardPins = (boardId) => {
   console.warn('soon to be board pins');
   $('body').on('click', '.card-body', (e) => {
     e.stopImmediatePropagation();
     $('#app').html('');
+
+    console.warn(boardId);
+
+    pinData.getBoardPins(boardId).then((response) => {
+      console.warn(response);
+      if (response.length) {
+        response.forEach((item) => {
+          $('#app').append(card.pinMaker(item));
+        });
+      } else {
+        $('#app').append('<h2>No pins related to this board!</h2>');
+      }
+    });
   });
 };
 
