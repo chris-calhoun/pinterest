ef0f5f4c8ff5ab55a588f4ff3ba18640ff5a567c
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-10 07:57:50 -0500

    fixed bug related to add pin and add board.

--- src/javascripts/helpers/data/boardData.js
+++ src/javascripts/helpers/data/boardData.js
@@ -3,29 +3,31 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
-const addBoard = (data) => axios
-  .post(`${baseUrl}/board.json`, data)
-  .then((response) => {
-    const update = { firebaseKey: response.data.name };
-    axios.patch(`${baseUrl}/board/${response.data.name}.json`, update);
-  })
-  .catch((error) => console.warn(error));
-
-const getUserBoards = (userUid) => new Promise((resolve, reject) => {
+const addBoard = (data) =>
   axios
-    .get(`${baseUrl}/board.json?orderBy="User_ID"&equalTo="${userUid}"`)
+    .post(`${baseUrl}/board.json`, data)
     .then((response) => {
-      const userBoards = response.data;
-      const boards = [];
-      if (userBoards) {
-        Object.keys(userBoards).forEach((boardId) => {
-          boards.push(userBoards[boardId]);
-        });
-      }
-      resolve(boards);
+      const update = { Board_Firebase_Key: response.data.name };
+      axios.patch(`${baseUrl}/board/${response.data.name}.json`, update);
     })
-    .catch((error) => reject(error));
-});
+    .catch((error) => console.warn(error));
+
+const getUserBoards = (userUid) =>
+  new Promise((resolve, reject) => {
+    axios
+      .get(`${baseUrl}/board.json?orderBy="User_ID"&equalTo="${userUid}"`)
+      .then((response) => {
+        const userBoards = response.data;
+        const boards = [];
+        if (userBoards) {
+          Object.keys(userBoards).forEach((boardId) => {
+            boards.push(userBoards[boardId]);
+          });
+        }
+        resolve(boards);
+      })
+      .catch((error) => reject(error));
+  });
 
 export default {
   getUserBoards,
