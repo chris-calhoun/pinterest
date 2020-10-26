b04af5e66038a15220da39ef2f9c51b3070b4347
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-25 21:56:52 -0500

    add view to update board.

---
+++ src/javascripts/components/views/updateBoardView.js
@@ -0,0 +1,11 @@
+import boardData from '../../helpers/data/boardData';
+import form from '../forms/updateBoardForm';
+
+const updateBoard = (boardId) => {
+  $('#app').html('<div class="boardForm" id="update-board"></div>');
+  boardData.getBoard(boardId).then((response) => {
+    form.updateBoardForm(response);
+  });
+};
+
+export default { updateBoard };
