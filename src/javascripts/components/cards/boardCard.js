43da5e7d1b6499d874afffd2d479f0891bf6ae74
Chris Calhoun <[ctcalhoun2@gmail.com]>
2020-10-27 19:41:09 -0500

    updated delete button styling.

--- src/javascripts/components/cards/boardCard.js
+++ src/javascripts/components/cards/boardCard.js
@@ -1,11 +1,11 @@
 const boardMaker = (boardObject) => {
   const domString = `<div id="${boardObject.Board_Firebase_Key}" class="card m-2" style="width: 18rem;">
-  <img id="${boardObject.Board_Firebase_Key}" class = "delete-board" viewBox="0 0 30 30" width="15" height="15" src="/src/svg/trash.svg" alt="icon name">
   <div class="card-board-body" id="${boardObject.Board_Firebase_Key}">
   <img class="card-img-top" src="${boardObject.Image_URL}" alt="Card image cap">
     <h5 class="card-title">${boardObject.Name}</h5>
     </div>
     <button type="button" id="${boardObject.Board_Firebase_Key}" class="btn btn-dark update-board">Update</button>
+    <button type="button" id="${boardObject.Board_Firebase_Key}" class="btn btn-danger delete-board">Delete</button>
   </div>`;
 
   return domString;
